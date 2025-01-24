26b36de8db877a187a3cd7c5b54cad226abc182f
Ahmed Tarek <ahmedtarekabd2002@gmail.com>
2025-01-25 00:04:37 +0200

    Remove io.h header in quadruple.h: This is a windows specific header and wasn't used. Which causes error when deploying to steamlit (Again Linux based system)

--- quadruple.h
+++ quadruple.h
@@ -5,16 +5,16 @@
 #include <stdlib.h>
 #include <string.h>
 #include "symbol_table.h"
-#include <io.h>
 #include <unistd.h>
 #include "utils/helper_functions.h"
-extern int yylineno;  
+extern int yylineno;
 // Define the quadruple structure
-typedef struct {
-    char operat[10];  // The operation (e.g., "+", "-", "=", "if")
-    char operand1[50];  // The first operand
-    char operand2[50];  // The second operand (optional)
-    char result[50];    // The result of the operation
+typedef struct
+{
+    char operat[10];   // The operation (e.g., "+", "-", "=", "if")
+    char operand1[50]; // The first operand
+    char operand2[50]; // The second operand (optional)
+    char result[50];   // The result of the operation
 } Quadruple;
 // Max number of quadruples
 #define MAX_QUADRUPLES 1000
@@ -25,21 +25,21 @@ extern int quadIndex;
 extern int tempCounter;
 // Function declarations
 SymbolTableEntry *addQuadruple(const char *operat, SymbolTableEntry *operand1, SymbolTableEntry *operand2);
-SymbolTableEntry *addQuadrupleFunctionCall(SymbolTableEntry *function_name,SymbolTableEntry **currentFunctionParams, int currentFunctionParamsCount);
-void addQuadrupleFunction(FunctionDef *functionDef , bool beforeSomeCode);
-void addQuadrupleLabel(SymbolTableEntry *condition , char * loopLabel , char* exitLabel, bool beforeSomeCode);
-void unmatchedIfQuadruple(SymbolTableEntry *condition , char * loopLabel , char* exitLabel, bool beforeSomeCode);
-void matchedIfQuadruple(char* exitLabel, bool beforeSomeCode);
-void switchcaseQuadruple(SymbolTableEntry *condition , char * nextLabel ,char* endLabel, bool beforeSomeCode, bool isEnd);
+SymbolTableEntry *addQuadrupleFunctionCall(SymbolTableEntry *function_name, SymbolTableEntry **currentFunctionParams, int currentFunctionParamsCount);
+void addQuadrupleFunction(FunctionDef *functionDef, bool beforeSomeCode);
+void addQuadrupleLabel(SymbolTableEntry *condition, char *loopLabel, char *exitLabel, bool beforeSomeCode);
+void unmatchedIfQuadruple(SymbolTableEntry *condition, char *loopLabel, char *exitLabel, bool beforeSomeCode);
+void matchedIfQuadruple(char *exitLabel, bool beforeSomeCode);
+void switchcaseQuadruple(SymbolTableEntry *condition, char *nextLabel, char *endLabel, bool beforeSomeCode, bool isEnd);
 void printQuadruples();
-void writeQuadrupleToFile(int i) ;
-void writeCommandToFile(char *command) ;
+void writeQuadrupleToFile(int i);
+void writeCommandToFile(char *command);
 void insertCommandBeforeEnd(const char *command);
 void printFileContents(const char *filename);
 void handleTypeConversion(const char *fromType, const char *toType, const char *varName);
-void semanticError(const char *s) ;
+void semanticError(const char *s);
 void copyString(char *dest, const char *src, size_t size);
-void checkQuadrupleStorageOverflow() ;
-const char* determineVarType(const char *operand1_type, const char *operand2_type, const char *operand1_name, const char *operand2_name);
+void checkQuadrupleStorageOverflow();
+const char *determineVarType(const char *operand1_type, const char *operand2_type, const char *operand1_name, const char *operand2_name);
 
 #endif // QUADRUPLE_H
